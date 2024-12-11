ba98bcaf4bc3f71e0ea8c98e1561b7890010ddad
zlevin1228 <zacharylevin1228@gmail.com>
2024-12-11 13:45:25 -0500

    Step Seven Mostly Complete, Step Eight Done

--- site.js
+++ site.js
@@ -33,7 +33,7 @@ const vue_app = Vue.createApp({
         return {
             // This holds your movies.json data.
             movies: [
-                  
+
             ],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
             title: "Zach's Top 8 Movies",
@@ -43,6 +43,55 @@ const vue_app = Vue.createApp({
     },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+            getMonthText(dateArray) {
+                  let month = ""
+                  switch (dateArray[1]) {
+                        case 1:
+                              month = "January"
+                              break
+                        case 2:
+                              month = "February"
+                              break
+                        case 3:
+                              month = "March"
+                              break
+                        case 4:
+                              month = "April"
+                              break
+                        case 5:
+                              month = "May"
+                              break
+                        case 6: 
+                              month = "June"
+                              break
+                        case 7:
+                              month = "July"
+                              break
+                        case 8:
+                              month = "August"
+                              break
+                        case 9:
+                              month = "September"
+                              break
+                        case 10:
+                              month = "October"
+                              break
+                        case 11:
+                              month = "November"
+                              break
+                        case 12:
+                              month = "December"
+                              break
+                  }
+                  return month + " " + dateArray[2] + ", " + dateArray[0]
+            },
+            posterClick(index) {
+                  if (this.movies[index].posterindex > this.movies[index].posters.length - 2) {
+                        this.movies[index].posterindex = 0
+                  } else {
+                        this.movies[index].posterindex++
+                  }
+            }
       }
 })
 
